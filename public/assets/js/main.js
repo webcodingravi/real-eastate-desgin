8eb7b0a67edf4d8c1e88249e358c2667a82ccdf0
Ravi kumar <rkconsultancy34@gmail.com>
2025-03-25 12:07:32 +0530

    new changing

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,43 @@
+ /****navbar scroll */
+
+let nav = document.querySelector(".navbar");
+let mybutton = document.getElementById("myBtn");
+window.onscroll = function() {
+ if(document.documentElement.scrollTop > 20) {
+     nav.classList.add('scroll-on');
+     mybutton.style.display = "block";
+ }else{
+  nav.classList.remove('scroll-on');
+  mybutton.style.display = "none";
+
+ }
+
+ /****navbar hide */
+
+ let navBar = document.querySelectorAll(".nav-link");
+ let Navcollapse = document.querySelector(".navbar-collapse.collapse");
+
+ navBar.forEach(function(a) {
+a.addEventListener("click", function() {
+    Navcollapse.classList.remove("show");
+})
+ });
+
+}
+ 
+
+// loader
+ let preloader = document.querySelector(".preloader");
+ window.addEventListener("load",function(){
+        preloader.style.display = 'none';
+ });
+ 
+ 
+ 
+ 
+ 
+ 
+ 
  /*****counter */
 
  document.addEventListener("DOMContentLoaded", () => {
